be80fa40c02cb9c21781ef8976caf369a17fe914
Douglas Skubisz <douglasskubisz@hotmail.com>
2022-08-17 21:40:49 -0300

    novo script com function

---
+++ 000-exercicios-adicionais/07-embaralhar-letras/script.js
@@ -0,0 +1,8 @@
+function transformar(){
+    let textArea = document.getElementById('textArea').values
+    
+    let resultado = document.getElementById('resultado')
+
+    resultado.innerHTML = textArea
+    //document.resultado.innerHTML = textArea
+}
