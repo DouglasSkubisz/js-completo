9397315242d5f92867930414dbcebe0616f5785b
Douglas Skubisz <douglasskubisz@hotmail.com>
2022-08-17 22:54:36 -0300

    laço for corrigido com -1

--- 000-exercicios-adicionais/07-embaralhar-letras/script.js
+++ 000-exercicios-adicionais/07-embaralhar-letras/script.js
@@ -1,10 +1,9 @@
 function processar(texto){
     let teste = texto
     let resultado = document.getElementById('resultado')
-    for (let i = teste.length; i >= 0 ; i--) {
+    for (let i = teste.length -1; i >= 0 ; i--) {
         resultado.innerHTML += teste[i]
         console.log(teste[i])
-        
     }
 
 }
